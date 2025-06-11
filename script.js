8bcd984123e0db3d0d2b06a861b64fcf70f788ec
Irham Rahmat Saleh <irhamrahmatsaleh1997@gmail.com>
2025-06-11 15:13:35 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('addTodoButton').addEventListener('click', function () {
+  const todoInput = document.getElementById('todoInput')
+  const todoText = todoInput.value.trim()
+  if (todoText) {
+    const li = document.createElement('li')
+    li.textContent = todoInput.value
+    document.getElementById('todoList').appendChild(li)
+    // todoInput.value = ''
+  }
+})
